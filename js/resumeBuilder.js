2f11471d6736316ba24b58ed9965dafd79dbd221
Filipe Alves Vaz <filipealvesvaz@gmail.com>
2017-10-26 17:48:17 -0200

    Add files via upload

---
+++ js/resumeBuilder.js
@@ -0,0 +1,170 @@
+var formattedName = HTMLheaderName.replace("%data%", "Filipe Vaz");
+var formattedRole = HTMLheaderRole.replace("%data%", " ");
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+var formattedbioPic = HTMLbioPic.replace("%data%", "images/fry.jpg");
+$("#header").append(formattedbioPic);
+var formattedwelcomeMsg = HTMLwelcomeMsg.replace("%data%","Welcome to my resume site!");
+$("#header").append(formattedwelcomeMsg);
+var formattedskillsStart = HTMLskillsStart;
+$("#header").append(formattedskillsStart);
+var formattedSkills = HTMLskills.replace("%data%", "HTML, CSS, JS");
+$("#header").append(formattedSkills);
+var formattedMobile = HTMLmobile.replace("%data%", "(27)99811-3589");
+$("#topContacts").prepend(formattedMobile);
+var formattedEmail = HTMLemail.replace("%data%", "filipealvesvaz@gmail.com");
+$("#topContacts").prepend(formattedEmail);
+var formattedGithub = HTMLgithub.replace("%data%", "filipealvesvaz");
+$("#topContacts").prepend(formattedGithub);
+var formattedTwitter = HTMLtwitter.replace("%data%", "@filipealvesvaz");
+$("#topContacts").prepend(formattedTwitter);
+var formattedLocation = HTMLlocation.replace("%data%", "Serra-ES");
+$("#topContacts").prepend(formattedLocation);
+
+ var work = {
+     "jobs": [
+     	{
+          "employer": "effe",
+          "title": "Developer" ,
+          "location": "Serra", 
+          "dates": "Currently",
+          "description": "Front-End Developer"
+        },
+        {
+          "employer": "SEDU",
+          "title": "Intructor" ,
+          "location": "Serra", 
+          "dates": "Currently",
+          "description": "Teaching programming classes"
+        },
+        {
+          "employer": "SENAI",
+          "title": "Intructor" ,
+          "location": "Serra", 
+          "dates": "2009 - 2015",
+          "description": "Teaching programming classes"
+        },     
+        {
+          "employer": "FABRA",
+          "title": "Professor" ,
+          "location": "Serra", 
+          "dates": "2011 - 2012",
+          "description": "Teaching programming classes"
+        },
+                 
+    ],
+
+    "display": function(){
+        $("#workExperience").append(HTMLworkStart)
+        work.jobs.forEach(function(job){
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", job.employer)
+        var formattedTitle = HTMLworkTitle.replace("%data%", job.title)
+        var formattedLocation = HTMLworkLocation.replace("%data%", job.location)
+        var formattedDates = HTMLworkDates.replace("%data%", job.dates)
+        var formattedDescription = HTMLworkDescription.replace("%data%", job.description)
+        $(".work-entry:last").append(formattedEmployer, formattedTitle, formattedLocation, formattedDates, formattedDescription)
+        })
+    }
+}
+work.display()
+
+var project = {
+	"projects": [
+		{
+		 "title": " ",
+		 "dates": " ",
+		 "description":"A website made with HTML",
+		 "images": ["images/site1.jpg"]
+     	 // "links": ["1/1.html"] 
+		},
+		{
+		 "title": " ",
+		 "dates": " ",
+		 "description":"A website made with HTML and CSS",
+		 "images": ["images/site2.jpg"]
+     	 // "links": ["2/2.html"]
+		},
+		{
+		 "title": " ",
+		 "dates": " ",
+		 "description":"A static site dynamically generated with Python",
+		 "images": ["images/site3.jpg"]
+      	 // "links": ["3/3.html"]
+		},
+	],
+
+
+	"display": function(){
+        $("#projects").append(HTMLprojectStart)
+        project.projects.forEach(function(project){
+        var formattedTitle = HTMLworkTitle.replace("%data%", project.title)
+        var formattedDates = HTMLworkDates.replace("%data%", project.dates)
+        var formattedDescription = HTMLworkDescription.replace("%data%", project.description)
+        var formattedprojectImage = HTMLprojectImage.replace("%data%", project.images)
+        var formattedprojectLink = HTMLprojectLink.replace("%data%", project.links)
+        $(".project-entry:last").append(formattedTitle, formattedDates, formattedDescription, formattedprojectImage) //, formattedprojectLink)
+        })
+    }
+}
+project.display()
+
+var education = {
+	"schools": [
+	{
+		"name": "SENAI",
+		"degree": "Degree",
+		"dates": 2014,
+		"location": "Rio de Janeiro",
+		"major": ["ED"],
+	
+	},
+    {
+      "name": "FAESA",
+      "degree": "Degree",
+      "dates": 2013,
+      "location": "Vitória",
+      "major": ["CS"],
+    
+    },
+    {
+      "name": "UNESC",
+      "degree": "Degree",
+      "dates": 2011,
+      "location": "Serra",
+      "major": ["CS"],
+    
+    },
+    {
+      "name": "CESA College",
+      "degree": "Bachelor",
+      "dates": 2008,
+      "location": "Serra",
+      "major": ["CS"],
+    
+    },
+		
+	],
+
+	
+	"display": function(){
+        $("#education").append(HTMLschoolStart)
+        education.schools.forEach(function(school){
+        var formattedName = HTMLschoolName.replace("%data%", school.name)
+        var formattedDegree = HTMLschoolDegree.replace("%data%", school.degree)
+        var formattedDates = HTMLschoolDates.replace("%data%", school.dates)
+        var formattedLocation = HTMLschoolLocation.replace("%data%", school.location)
+        var formattedMajor = HTMLschoolMajor.replace("%data%", school.major)
+        $(".education-entry:last").append(formattedName, formattedDegree, formattedDates,formattedLocation, formattedMajor)
+
+        
+        })
+    }
+}
+education.display()
+
+
+$("#footerContacts").prepend(formattedMobile);
+$("#footerContacts").prepend(formattedEmail);
+$("#footerContacts").prepend(formattedGithub);
+$("#footerContacts").prepend(formattedTwitter);
+$("#footerContacts").prepend(formattedLocation);
